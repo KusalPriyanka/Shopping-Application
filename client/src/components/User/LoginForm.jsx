be1f5792dc6a60fa3b6c29e7154b477a0e12f45e
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-04-22 23:52:56 +0530

    Crete login form to the user

---
+++ client/src/components/User/LoginForm.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <div className="row m-5">
+        <div className="col-md-12">
+          <div className="card">
+            <div className="card-header">User Login</div>
+            <div className="card-body"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
