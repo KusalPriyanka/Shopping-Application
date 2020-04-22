9600a02ac67ad62985d15d1ef870473e65bb7049
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-04-22 23:51:28 +0530

    Configure basic routes in client app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Navigation from "./components/Shared/Navigation";
+
+import LoginForm from "./components/User/LoginForm";
+import RegisterForm from "./components/User/RegisterForm";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <div className="container-fluid">
+        <Router>
+          <Navigation />
+          <Switch>
+            {/* <Route path="/" exact component={LoginForm} /> */}
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
