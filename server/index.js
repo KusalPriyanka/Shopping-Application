85467d89ddd68917bb6fda2086bc530255959390
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-04-22 09:27:45 +0530

    Update index file (configure server and connected to the db)

---
+++ server/index.js
@@ -0,0 +1,30 @@
+// Import Packages
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+// Import routes
+const UserRoutes = require("./routes/UserRoutes");
+
+dotenv.config();
+
+// Connect DB
+mongoose.connect(
+  process.env.DB_CONTEXT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to the mongodb");
+  }
+);
+
+// Middlewares
+app.use(express.json());
+
+// Config routes
+app.use("/api/users", UserRoutes);
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server is up and running on server on 3000");
+});
