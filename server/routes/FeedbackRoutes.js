abadc34b6223c364ee438cc97775cc03996b34f5
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-05-12 20:45:02 +0530

    add feedback method added

--- server/routes/FeedbackRoutes.js
+++ server/routes/FeedbackRoutes.js
@@ -12,4 +12,31 @@ router.get("/", async (req, res) => {
   res.send(feedbacks);
 });
 
+router.post("/add", async (req, res) => {
+  // Validation
+  const { error } = feedbackValidation(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+
+  const feedbackExist = await FeedbackModel.findOne({
+    $and: [{ userId: userId }, { productId: req.body.productId }],
+  });
+
+  if (feedbackExist)
+    return res.status(404).send("Already published the feedback!");
+
+  const feedback = new FeedbackModel({
+    productId: req.body.productId,
+    rating: req.body.rating,
+    feedback: req.body.feedback,
+    userId: userId,
+  });
+
+  try {
+    await feedback.save();
+    res.send({ feedbackId: feedback._id });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
 module.exports = router;
