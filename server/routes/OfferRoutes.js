38ff1e1fdb011b226b92cb624b69e895b946b9de
AshenSenevirathne <ashensachin@gmail.com>
2020-05-17 09:27:37 +0530

    Offer Routes Updated

--- server/routes/OfferRoutes.js
+++ server/routes/OfferRoutes.js
@@ -26,15 +26,15 @@ router.get("/GetProductOffer/:id", (req, res) => {
 /*Router To Add Offers*/
 router.post("/AddOffer", (req, res) =>{
 
+    console.log("sfdsdfsdfsdfsdsdfsdf")
     let offer  = new Offer({
         offerName : req.body.offerName,
         offerType : req.body.offerType,
         offerAmount : req.body.offerAmount,
         productCategory : req.body.productCategory,
         products : req.body.products,
-        offerImageURL : req.body.offerImageURL,
         offerCode : req.body.offerCode,
-        offerEndDate : req.body.offerEndDate
+        // offerEndDate : req.body.offerEndDate
     });
 
     offer.save()
