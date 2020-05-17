3e008b77e8bcb9ce4aee264ec542fad63bcb98d9
Dimuthu <dimuthuc2@gmail.com>
2020-05-17 18:41:34 +0530

    Added nodemailer to server

--- server/routes/StoreManagerRoutes.js
+++ server/routes/StoreManagerRoutes.js
@@ -1,5 +1,6 @@
 const route = require("express").Router();
 const StoreManager = require("../model/StoreManeger");
+var nodemailer = require('nodemailer');
 
 /*Router to get all store managers from db*/
 route.get("/", (req, res) =>{
@@ -17,6 +18,7 @@ route.get("/:id", (req, res) =>{
 
 /*Router to add store manager to db*/
 route.post("/AddStoreManager", (req, res) =>{
+
     let sm = new StoreManager({
         userName: req.body.userName,
         userAddress: req.body.userAddress,
@@ -26,8 +28,37 @@ route.post("/AddStoreManager", (req, res) =>{
     });
 
     sm.save()
-        .then((sm) => res.send(sm))
+        .then((sm) => {
+            res.send(sm);
+            
+            //send email to the newly created store manager
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                user: 'hotelbluedragon@gmail.com',
+                pass: 'mad@2019'
+                }
+            });
+            
+            var mailOptions = {
+                from: '"IshopPlaza" <IshopPlaza@gmail.com>',
+                to: `${req.body.userEmail}`,
+                subject: 'IshopPlaza',
+                text: 'Your account has been created. Password: abc123+',
+                html: `<b>Welcome to IshopPlaza! </b><br/><br/><br/>Your account has been created.<br/><br/>User name: ${req.body.userName}<br/>Password: abc123+`,
+            };
+            
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                console.log(error);
+                } else {
+                console.log('Email sent: ' + info.response);
+                }
+            }); 
+            
+        })
         .catch((err) => res.status(400).send(`Error: ${err}`))
+
 });
 
 /*Router to delete store manager from db*/
