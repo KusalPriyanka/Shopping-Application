a5b8e9e1d1c1de2f4f17ef5979ac4fd32a2a7c0f
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-05-20 10:58:50 +0530

    added feedback component

--- client/src/components/MainProductView/MainProductViewContainer.jsx
+++ client/src/components/MainProductView/MainProductViewContainer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import MainProductView from "./MainProductView";
+import Feedback from "../Feedback/Feedback";
 
 const MainProductViewContainer = (props) => {
-    return(
-        <React.Fragment>
-            <MainProductView productId={props.match.params.id}/>
-            <h1>FeedBacks</h1>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <MainProductView productId={props.match.params.id} />
+      <Feedback productId={props.match.params.id} />
+    </React.Fragment>
+  );
+};
 
 export default MainProductViewContainer;
