08623e9ba91de996def96de364e03eec2891590e
AnjaniWelagedara <anjaniwelagedara3@gmail.com>
2020-05-20 12:44:09 +0530

    ShoppingCartItem file added

---
+++ client/src/components/ShoppingCart/ShopingCartItem.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class ShoppingCartItem extends Component{
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
