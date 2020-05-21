e816569fcc1619ee3d2c06fd03f5f516033a64aa
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-05-21 21:27:11 +0530

    update index page adding routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./css/index.css";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
-  <React.Fragment>
-    <App />
-  </React.Fragment>,
+  <Router history={history}>
+    <React.Fragment>
+      <App />
+    </React.Fragment>
+  </Router>,
   document.getElementById("root")
 );
 
