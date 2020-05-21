beff8ad94b8f9d84091334c4ba9c66221834462a
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-05-21 18:49:04 +0530

    add api service for employee login

---
+++ client/src/services/ApiEmployeeServices/EmployeeServices.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const empLogin = (empObj) => {
+  const apiURL =
+    process.env.apiURL || "http://localhost:8080/api/EmployeeLogin/";
+
+  return axios
+    .post(apiURL, empObj)
+    .then((res) => {
+      return {
+        status: true,
+        data: res,
+      };
+    })
+    .catch((err) => {
+      return {
+        status: false,
+        data: err,
+      };
+    });
+};
