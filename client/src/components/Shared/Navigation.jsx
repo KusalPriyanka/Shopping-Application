5449d15e37cf21101a4791d308ae9d3692c3b6fb
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-05-21 21:26:12 +0530

    added method to appbar visibility condition

--- client/src/components/Shared/Navigation.jsx
+++ client/src/components/Shared/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import {makeStyles} from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Toolbar from "@material-ui/core/Toolbar";
 import AppBar from "@material-ui/core/AppBar";
@@ -17,106 +17,117 @@ import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 
 const useStyles = makeStyles((theme) => ({
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    appBar: {
-        position: "relative",
-    },
-    list: {
-        width: 250,
-    },
-    fullList: {
-        width: "auto",
-    },
-    title: {
-        flexGrow: 1,
-    },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  appBar: {
+    position: "relative",
+  },
+  appBarVisible: {
+    display: "none",
+  },
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+  title: {
+    flexGrow: 1,
+  },
 }));
 
 const Navigation = (props) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [state, setState] = React.useState(false);
+  const [state, setState] = React.useState(false);
 
-    const toggleDrawer = (open) => (event) => {
-        if (
-            event.type === "keydown" &&
-            (event.key === "Tab" || event.key === "Shift")
-        ) {
-            return;
-        }
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
 
-        setState(open);
-    };
+    setState(open);
+  };
 
-    const list = () => (
-        <div
-            className={clsx(classes.list, {
-                [classes.fullList]: "left" === "top" || "left" === "bottom",
-            })}
-            role="presentation"
-            onClick={toggleDrawer(false)}
-            onKeyDown={toggleDrawer(false)}
-        >
-            <Divider/>
-            <List>
-                {props.categories.map((category) => {
-                    return (
-                        <React.Fragment key={category}>
-                            <ListItem button component={Link} to={`/category/${category.CategoryName}`}>
-                                <ListItemAvatar>
-                                    <Avatar
-                                        alt="Remy Sharp"
-                                        src={`https://source.unsplash.com/featured/?${category.CategoryName}`}
-                                    />
-                                </ListItemAvatar>
-                                <ListItemText primary={category.CategoryName}/>
-                            </ListItem>
-                            <Divider/>
-                        </React.Fragment>
-                    );
-                })}
-            </List>
-        </div>
-    );
+  const list = () => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: "left" === "top" || "left" === "bottom",
+      })}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <Divider />
+      <List>
+        {props.categories.map((category) => {
+          return (
+            <React.Fragment key={category}>
+              <ListItem
+                button
+                component={Link}
+                to={`/category/${category.CategoryName}`}
+              >
+                <ListItemAvatar>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={`https://source.unsplash.com/featured/?${category.CategoryName}`}
+                  />
+                </ListItemAvatar>
+                <ListItemText primary={category.CategoryName} />
+              </ListItem>
+              <Divider />
+            </React.Fragment>
+          );
+        })}
+      </List>
+    </div>
+  );
 
-    return (
-        <React.Fragment>
-            <AppBar position="static" color="default" className={classes.appBar}>
-                <Toolbar>
-                    <IconButton
-                        edge="start"
-                        className={classes.menuButton}
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={toggleDrawer(true)}
-                        style={{outline: "none"}}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h6" color="inherit" className={classes.title} >
-                        IShop Plaza
-                    </Typography>
-                    <Button component={Link} to="/" color="inherit">
-                        Home
-                    </Button>
-                    <Button component={Link} to="/login" color="inherit">
-                        Login
-                    </Button>
-                    <Button component={Link} to="/register" color="inherit">
-                        Register
-                    </Button>
-                    <Button component={Link} to="/storeManager" color="inherit">
-                        Store Manager
-                    </Button>
-                </Toolbar>
-            </AppBar>
-            <Drawer anchor={"left"} open={state} onClose={toggleDrawer(false)}>
-                {list("left")}
-            </Drawer>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <AppBar
+        position="static"
+        color="default"
+        className={props.visibility ? classes.appBar : classes.appBarVisible}
+      >
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer(true)}
+            style={{ outline: "none" }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.title}>
+            IShop Plaza
+          </Typography>
+          <Button component={Link} to="/" color="inherit">
+            Home
+          </Button>
+          <Button component={Link} to="/login" color="inherit">
+            Login
+          </Button>
+          <Button component={Link} to="/register" color="inherit">
+            Register
+          </Button>
+          <Button component={Link} to="/storeManager" color="inherit">
+            Store Manager
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor={"left"} open={state} onClose={toggleDrawer(false)}>
+        {list("left")}
+      </Drawer>
+    </React.Fragment>
+  );
 };
 
 export default Navigation;
