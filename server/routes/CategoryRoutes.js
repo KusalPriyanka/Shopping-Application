e3a8c0a9b3f7a8014d2850e81e8c34ad4f10ab98
Dimuthu <dimuthuc2@gmail.com>
2020-05-23 23:18:25 +0530

    Added verifyToken to categoryRoutes

--- server/routes/CategoryRoutes.js
+++ server/routes/CategoryRoutes.js
@@ -1,44 +1,54 @@
 const router = require("express").Router();
 const Categories = require('../model/Categories');
+const verifyToken=require("./Authentication/VerifyToken");
 
 /*Router to get all Categories from db*/
-router.get("/", (req, res) => {
-    Categories.find()
+router.get("/", verifyToken, async (req, res) => {
+    if(!req.user.userRole === 'admin')
+        return res.status(401).send("Access Denied!");
+
+        await Categories.find()
         .then(Categories => res.send(Categories))
         .catch(err => res.status(400).send('Error: ' + err))
 
 });
 
 /*Router to get Category by id from db*/
-router.get("/:id", (req, res) => {
-    Categories.findById(req.params.id)
+router.get("/:id", verifyToken, async (req, res) => {
+
+    await Categories.findById(req.params.id)
         .then((Category) => res.send(Category))
         .catch(err => res.status(400).send("Error : " + err))
 });
 
 /*Router to add Categories to db*/
-router.post('/AddCategory', (req, res) => {
+router.post('/AddCategory',verifyToken, async (req, res) => {
+
     let Category = new Categories({
         CategoryName: req.body.CategoryName,
         categoryImageURL: req.body.categoryImageURL,
         categoryDescription: req.body.categoryDescription,
     });
 
-    Category.save()
-        .then((Category) => res.send(Category))
-        .catch(err => res.status(400).send('Error: ' + err))
+    try {
+        await Category.save();
+        res.send(Category);
+      } catch (err) {
+        res.status(400).send('Error: ' + err);
+      }
+
 });
 
 /*Router to delete Categories from db*/
-router.delete("/DeleteCategory/:id", (req, res) => {
-    Categories.findByIdAndDelete(req.params.id)
+router.delete("/DeleteCategory/:id",verifyToken, async (req, res) => {
+    await Categories.findByIdAndDelete(req.params.id)
         .then(() => res.send("Successfully Deleted!. Category ID: ( " + req.params.id + " )"))
         .catch(err => res.status(400).send("Error : " + err))
 });
 
 /*Router to Update Categories*/
-router.put("/UpdateCategory/:id", (req, res) => {
-    Categories.findById(req.params.id)
+router.put("/UpdateCategory/:id", verifyToken, async (req, res) => {
+    await Categories.findById(req.params.id)
         .then(Category => {
             Category.CategoryName = req.body.CategoryName;
             Category.categoryImageURL = req.body.categoryImageURL;
