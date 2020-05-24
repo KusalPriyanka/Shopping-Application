8af2f7545f5ad59f3dbe7a5cdecdf4706ee4ebf7
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-05-24 13:51:44 +0530

    update urls

--- client/src/App.js
+++ client/src/App.js
@@ -21,10 +21,8 @@ function App({ location }) {
   const [currentPath, setCurrentPath] = useState(location.pathname);
 
   const getCategoryFromDB = () => {
-    const apiURL =
-      process.env.apiURL || "http://localhost:8080/" + "api/Categories/";
     axios
-      .get(apiURL)
+      .get("api/Categories/")
       .then((res) => {
         setCategories(res.data);
       })
