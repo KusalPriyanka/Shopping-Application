f816ba283ecbf70d06f59889d6c223be493dc14e
AshenSenevirathne <ashensachin@gmail.com>
2020-05-24 09:22:06 +0530

    function provide to child to manage redirect

--- client/src/components/StoreManager/StoreManagerDashBoard.jsx
+++ client/src/components/StoreManager/StoreManagerDashBoard.jsx
@@ -6,15 +6,19 @@ import AllProducts from "./ProductView/AllProducts";
 import AddProduct from "./AddProduct/AddProduct";
 import AllDiscounts from "./Discounts/AllDiscounts";
 import UpdateProduct from "./UpdateProduct/UpdateProduct";
+import axios from "axios"
 
-const StoreManagerDashBoard = ({ location }) => {
+const StoreManagerDashBoard = ({location}) => {
 
     const history = useHistory();
     const [currentPath, setCurrentPath] = useState(location.pathname);
     const checkAuthentication = () => {
         if (!localStorage.getItem("emp")) {
             history.push("/employee")
-
+        } else {
+            axios.defaults.headers.common["auth-token"] = JSON.parse(
+                localStorage.getItem("emp")
+            ).empToken;
         }
     }
 
@@ -23,16 +27,20 @@ const StoreManagerDashBoard = ({ location }) => {
         setCurrentPath(location.pathname);
     }, [currentPath]);
 
+    const removeUser = () => {
+        localStorage.removeItem("emp")
+        history.push("/employee")
+    }
 
     return (
         <React.Fragment>
             <Router>
-                <TopSearchAppBar/>
+                <TopSearchAppBar removeUser={removeUser}/>
                 <Switch>
-                    <Route path="/storeManager" exact component={AllProducts}/>
-                    <Route path="/storeManager/addProducts" component={AddProduct}/>
+                    <Route path="/storeManager" exact component={() => <AllProducts removeUser={removeUser}/>}/>
+                    <Route path="/storeManager/addProducts" component={() => <AddProduct removeUser={removeUser}/>}/>
                     <Route path="/storeManager/updateProducts/:id" component={UpdateProduct}/>
-                    <Route path="/storeManager/allDiscounts" component={AllDiscounts}/>
+                    <Route path="/storeManager/allDiscounts" component={() => <AllDiscounts removeUser={removeUser}/>}/>
                 </Switch>
                 <OpenIconSpeedDial/>
             </Router>
