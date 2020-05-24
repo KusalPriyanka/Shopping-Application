ceb739e38d0b12f4bdf3edfa343364f1edadeec8
AnjaniWelagedara <anjaniwelagedara3@gmail.com>
2020-05-24 11:44:04 +0530

    modified file

--- client/src/components/ShoppingCart/GridListCart.jsx
+++ client/src/components/ShoppingCart/GridListCart.jsx
@@ -36,25 +36,9 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-/**
- * The example data is structured as follows:
- *
- * import image from 'path/to/image.jpg';
- * [etc...]
- *
- * const tileData = [
- *   {
- *     img: image,
- *     title: 'Image',
- *     author: 'author',
- *   },
- *   {
- *     [etc...]
- *   },
- * ];
- */
 
 
+/*Taking images for the grid list*/
 const tileData = [
     {
         img: carthree,
