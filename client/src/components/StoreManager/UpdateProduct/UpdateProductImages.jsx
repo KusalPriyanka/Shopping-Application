671659b55d3408cef79a2690dbf2c956e106cafa
AshenSenevirathne <ashensachin@gmail.com>
2020-05-24 09:25:29 +0530

    Product Update Function Created

--- client/src/components/StoreManager/UpdateProduct/UpdateProductImages.jsx
+++ client/src/components/StoreManager/UpdateProduct/UpdateProductImages.jsx
@@ -1,46 +1,82 @@
 import React from 'react';
 import {DropzoneArea} from 'material-ui-dropzone';
+import Swal from "sweetalert2";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ListItem from "@material-ui/core/ListItem";
 
-class UpdateProductDetails extends React.Component {
+class AddProductImages extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedFile: [],
+            selectedFile: this.props.selectedFile,
         };
 
         this.handleChange = this.handleChange.bind(this);
     }
 
+    alertMsg = (icon, title, text) => {
+        Swal.fire({
+            icon: icon,
+            title: title,
+            text: text,
+        });
+    }
+
     handleChange(files) {
+
         this.setState({
             selectedFile: files
         });
         this.props.updateSelectedFile(files);
     }
 
+    checkImageSize = () => {
+        if (this.state.selectedFile.length === 5) {
+            return true
+        } else {
+            this.alertMsg("error", "Oooooooooooopz!", "Please upload 5 images.")
+            return false
+        }
+    }
+
     componentWillUnmount() {
         this.props.updateSelectedFile(this.state.selectedFile)
     }
 
+
     render() {
         return (
             <React.Fragment>
-                <DropzoneArea
-                    onChange={files =>  this.handleChange(files)}
-                    acceptedFiles={['image/jpeg', 'image/png', 'image/bmp']}
-                    filesLimit={5}
-                    maxFileSize={5000000}
-                    dropzoneText={'Please Upload Images For Product'}
-                    showPreviews={false}
-                    showFileNamesInPreview={true}
-                    showPreviewsInDropzone={true}
-                    previewText={''}
+                {this.state.selectedFile.map(image => {
+                    return (<ListItem key={image} >
+                        <ListItemAvatar>
+                            <Avatar
+                                src={image}
+                            />
+                        </ListItemAvatar>
+                        {console.log(image)}
+                        <ListItemText
+                            secondary={image}
+                            dasfsdfsd
+                        />
+                        <ListItemIcon>
+                            <IconButton aria-label="delete" >
+                                <DeleteIcon fontSize="default" color={"error"}/>
+                            </IconButton>
+                        </ListItemIcon>
 
-
-                />
+                    </ListItem>)
+                    alert(image)
+                    }
+                )}
             </React.Fragment>
         )
     }
 };
 
-export default UpdateProductDetails;
+export default AddProductImages;
--- client/src/components/StoreManager/UpdateProduct/UpdateProductSize.jsx
+++ client/src/components/StoreManager/UpdateProduct/UpdateProductSize.jsx
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import MaterialTable from 'material-table';
 import ChipInput from 'material-ui-chip-input'
+import {productSizeValidation} from "../ProductView/../../../Validations/ProductValidation"
+import Swal from "sweetalert2";
 
 
-export default class UpdateProductSize extends Component{
+export default class SizeAndPrice extends Component {
     constructor(props) {
         super(props);
         this.state = {
             data: this.props.sizeAndPrice,
-            sizes : this.props.sizes,
+            sizes: this.props.sizes,
         }
         this.getColumns = this.getColumns.bind(this)
         this.addChips = this.addChips.bind(this)
@@ -16,19 +18,18 @@ export default class UpdateProductSize extends Component{
     }
 
 
-
     componentWillUnmount() {
         this.props.updateSizeAndPrice(this.state.data, this.state.sizes)
     }
 
-    getColumns(){
+    getColumns() {
         let sizesForTable = {};
         this.state.sizes.map(size => {
             Object.defineProperty(sizesForTable, size, {
                 value: size,
-                writable : true,
-                enumerable : true,
-                configurable : true
+                writable: true,
+                enumerable: true,
+                configurable: true
             });
         })
         let columns = [
@@ -45,26 +46,70 @@ export default class UpdateProductSize extends Component{
         return columns;
     }
 
-    addChips(chips){
+    addChips(chips) {
         let sizes = this.state.sizes;
         sizes.push(chips)
         this.setState({
-            sizes : sizes
+            sizes: sizes
         })
     }
-    handleDeleteChip(chip, index){
-        let sizes = this.state.sizes;
-        sizes.splice(index, 1);
-        this.setState({
-            sizes : sizes
+
+    handleDeleteChip(chip, index) {
+        let state = false
+
+        this.state.data.map(item =>{
+            if (item.productSize === chip){
+                state = true
+            }
         })
+
+        if(!state){
+            let sizes = this.state.sizes;
+            sizes.splice(index, 1);
+            this.setState({
+                sizes: sizes
+            })
+        }else {
+            this.alertMsg("error", "Oooooooooooopz!", "This Size have Added Colors.If You want To delete please remove Color")
+        }
+
+    }
+
+    alertMsg = (icon, title, text) => {
+        Swal.fire({
+            icon: icon,
+            title: title,
+            text: text,
+        });
+    }
+
+    checkSize = () => {
+        let unUsedSizes = this.state.sizes;
+
+        this.state.data.map(item => {
+            unUsedSizes = unUsedSizes.filter(size => {
+                return  size !== item.productSize
+            })
+        })
+
+        if(unUsedSizes.length !== 0){
+            this.alertMsg("error", "Oooooooooooopz!", "Please Remove Unused Sizes.")
+            return false
+        }
+
+        if(this.state.data.length !== 0){
+            return true
+        }else {
+            this.alertMsg("error", "Oooooooooooopz!", "Please Add Color And Quantity With Size.")
+            return false
+        }
     }
 
     render() {
 
         return (
             <React.Fragment>
-                <div style={{marginBottom : "20px", paddingLeft:"20px", paddingRight:"20px", marginTop : "-10px"}}>
+                <div style={{marginBottom: "20px", paddingLeft: "20px", paddingRight: "20px", marginTop: "-10px"}}>
                     <ChipInput
                         required
                         label={"Please add Product Sizes"}
@@ -75,56 +120,61 @@ export default class UpdateProductSize extends Component{
                         onDelete={(chip, index) => this.handleDeleteChip(chip, index)}
                     />
                 </div>
-            <MaterialTable
-                title="Product Sizes"
-                columns={this.getColumns()}
-                options={{
-                    actionsColumnIndex: -1,
-                    headerStyle: {
-                        backgroundColor: '#34569b',
-                        color: '#FFF'
-                    },
-                }}
+                <MaterialTable
+                    title="Product Sizes"
+                    columns={this.getColumns()}
+                    options={{
+                        actionsColumnIndex: -1,
+                        headerStyle: {
+                            backgroundColor: '#34569b',
+                            color: '#FFF'
+                        },
+                    }}
 
-                data={this.state.data}
-                editable={{
-                    onRowAdd: (newData) =>
-                        new Promise((resolve) => {
-                            setTimeout(() => {
-                                resolve();
-                                this.setState((prevState) => {
-                                    const data = [...prevState.data];
-                                    data.push(newData);
-                                    return {...prevState, data};
-                                });
-                            }, 600);
-                        }),
-                    onRowUpdate: (newData, oldData) =>
-                        new Promise((resolve) => {
-                            setTimeout(() => {
-                                resolve();
-                                if (oldData) {
+                    data={this.state.data}
+                    editable={{
+                        onRowAdd: (newData) =>
+                            new Promise((resolve) => {
+                                setTimeout(() => {
+                                    resolve();
                                     this.setState((prevState) => {
                                         const data = [...prevState.data];
-                                        data[data.indexOf(oldData)] = newData;
+                                        if (productSizeValidation(newData)) {
+                                            data.push(newData);
+                                        }
                                         return {...prevState, data};
                                     });
-                                }
-                            }, 600);
-                        }),
-                    onRowDelete: (oldData) =>
-                        new Promise((resolve) => {
-                            setTimeout(() => {
-                                resolve();
-                                this.setState((prevState) => {
-                                    const data = [...prevState.data];
-                                    data.splice(data.indexOf(oldData), 1);
-                                    return {...prevState, data};
-                                });
-                            }, 600);
-                        }),
-                }}
-            />
+                                }, 400);
+                            }),
+                        onRowUpdate: (newData, oldData) =>
+                            new Promise((resolve) => {
+                                setTimeout(() => {
+                                    resolve();
+                                    if (oldData) {
+                                        this.setState((prevState) => {
+                                            const data = [...prevState.data];
+                                            if (productSizeValidation(newData)) {
+                                                data[data.indexOf(oldData)] = newData;
+                                            }
+
+                                            return {...prevState, data};
+                                        });
+                                    }
+                                }, 600);
+                            }),
+                        onRowDelete: (oldData) =>
+                            new Promise((resolve) => {
+                                setTimeout(() => {
+                                    resolve();
+                                    this.setState((prevState) => {
+                                        const data = [...prevState.data];
+                                        data.splice(data.indexOf(oldData), 1);
+                                        return {...prevState, data};
+                                    });
+                                }, 600);
+                            }),
+                    }}
+                />
             </React.Fragment>
 
         );
