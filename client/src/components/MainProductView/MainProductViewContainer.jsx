6ef92fd30af4473a724559b384969c25ac687dcb
KusalPriyanka <kusalpriyanka782@gmail.com>
2020-05-24 19:51:50 +0530

    added if user not logged redirect to the login

--- client/src/components/MainProductView/MainProductViewContainer.jsx
+++ client/src/components/MainProductView/MainProductViewContainer.jsx
@@ -2,15 +2,33 @@ import React from "react";
 import MainProductView from "./MainProductView";
 import Feedback from "../Feedback/Feedback";
 import HomeNavigator from "../Shared/HomeNavigator";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const MainProductViewContainer = (props) => {
-    return (
-        <React.Fragment>
-            <MainProductView productId={props.match.params.id}/>
-            <Feedback productId={props.match.params.id}/>
-            <HomeNavigator/>
-        </React.Fragment>
-    );
+  const history = useHistory();
+  const [userLogIn, setUserLogIn] = useState(false);
+
+  useEffect(() => {
+    if (!localStorage.getItem("user")) {
+      history.push("/login");
+      setUserLogIn(false);
+    }
+    setUserLogIn(true);
+  }, []);
+
+  return (
+    <React.Fragment>
+      {userLogIn ? (
+        <div>
+          <MainProductView productId={props.match.params.id} />
+          <Feedback productId={props.match.params.id} />
+          <HomeNavigator />
+        </div>
+      ) : null}
+    </React.Fragment>
+  );
 };
 
 export default MainProductViewContainer;
